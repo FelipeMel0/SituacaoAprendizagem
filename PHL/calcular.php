8ae03d9ac6d7e4febc10c2c5befa16a4b533abe4
FELIPE DE MELO FERNANDES <21192908@CFP127ALUNO.LOCAL>
2021-08-06 10:36:53 -0300

    Atividades Concluídas

---
+++ PHL/calcular.php
@@ -0,0 +1,22 @@
+<style>
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-family: 'Roboto Condensed', sans-serif;
+        
+    }
+</style>
+<?php
+if (isset($_REQUEST["cidadeOrigem"]) && isset($_REQUEST["cidadeDestino"]) && isset ($_REQUEST["quantidadeKm"]) && isset ($_REQUEST["quantidadePedagios"])){
+    $cidadeOrigem = $_REQUEST["cidadeOrigem"];
+    $cidadeDestino = $_REQUEST["cidadeDestino"];
+    $quantidadeKm = $_REQUEST["quantidadeKm"];
+    $quantidadePedagios = $_REQUEST["quantidadePedagios"];
+    $soma = ($quantidadePedagios * 9.40) + ($quantidadeKm * 6);
+
+    echo "<h1>A viagem de $cidadeOrigem para $cidadeDestino irá custar o valor total de R$</h1>";
+    echo "<h1 style = 'color: black; font-size: 60px'>$soma</h1>";
+} else{
+    echo "Você não preencheu os dados corretamente";
+}
---
+++ Seneletro/calcular.php
@@ -0,0 +1,46 @@
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        overflow-y: hidden;
+        font-family: 'Roboto Condensed', sans-serif;
+        
+    }
+</style>
+<?php
+
+if(isset($_REQUEST["nomeCliente"]) && isset($_REQUEST["endereco"]) && isset($_REQUEST["kwh"])){
+    $nomeCliente = $_REQUEST["nomeCliente"];
+    $endereco = $_REQUEST["endereco"];
+    $kwh = $_REQUEST["kwh"];
+
+    if($kwh > 120){
+        $valor = $kwh * 0.42;
+
+        echo "<p style='font-size: 25px; font-weight: bold; margin-bottom: 5px;'>Conta de luz de $nomeCliente</p>";
+        echo "<br>";
+        echo "<p style='font-size: 15px;'>$endereco";
+        echo "<p style='font-size: 25px; color: red;'>Consumo: $kwh kWh </p>";
+        echo "<br>";
+        echo "Valor a pagar: ";
+        echo "<p style= 'font-size: 40px'>R$$valor</p>";
+        echo "<br>";
+    }
+    else{
+        $valor = $kwh * 0.32;
+
+        echo "<p style='font-size: 25px; font-weight: bold;'>Conta de luz de $nomeCliente</p>";
+        echo "<br>";
+        echo "<p style='font-size: 15px;'>$endereco";
+        echo "<p style='font-size: 25px; color: yellowgreen;'>Consumo: $kwh kWh </p>";
+        echo "<br>";
+        echo "Valor a pagar: ";
+        echo "<p style= 'font-size: 40px'>R$$valor</p>";
+        echo "<br>";
+        echo "<p style= 'font-size: 20px'>Obrigado por economizar! *beijo na tua alma*";
+    
+    }
+}
