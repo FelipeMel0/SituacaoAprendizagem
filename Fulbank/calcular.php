14aaf2f997b2b26299ab9ab4811e23a2500eaa03
FelipeMel0 <felipedemfernandes@gmail.com>
2021-08-12 18:41:01 -0300

    Desenvolvendo PHP

--- Fulbank/calcular.php
+++ Fulbank/calcular.php
@@ -1,16 +1,17 @@
 <?php
 
-if(isset($_REQUEST["nome"]) && isset($_REQUEST["salario"]) && isset($_REQUEST["cargos"])){
+if(isset($_REQUEST["nome"]) && isset($_REQUEST["salario"]) && isset($_REQUEST["cargos"]) && isset($_REQUEST["opcoes"])){
     $nome = $_REQUEST["nome"];
     $salario = $_REQUEST["salario"];
     $cargos = $_REQUEST["cargos"];
+    $genero = $_REQUEST["opcoes"];
     
     if($salario > 5000){
         $total = $salario * 110/100;
-        echo "<h1>O $nome passará a receber R$$total, no cargo de $cargos</h1>";
+        echo "<h1>A partir de alguns cálculos, $nome passará a receber R$$total, no cargo de $cargos. Aparentemente você também é do gênero '$genero'. Maneiro 👍</h1>";
     }else{
         $total = $salario * 120/100;
-        echo "<h1>O $nome passará a receber R$$total, no cargo de $cargos </h1>";
+        echo "<h1>A partir de alguns cálculos, $nome passará a receber R$$total, no cargo de $cargos. Aparentemente você também é do gênero '$genero'. Maneiro 👍 </h1>";
     }
 
-}
+} 
