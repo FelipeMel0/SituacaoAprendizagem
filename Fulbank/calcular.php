66092e534c72a647e660f38daa8b6bc45d005e05
FELIPE DE MELO FERNANDES <21192908@CFP127ALUNO.LOCAL>
2021-08-13 09:01:26 -0300

    Atualização no CSS

--- Fulbank/calcular.php
+++ Fulbank/calcular.php
@@ -5,23 +5,22 @@
         justify-content: center;
         font-family: 'Roboto Condensed', sans-serif;
         color: white;
-        background-color: rgb(255, 173, 20); 
+        background-color: rgb(255, 173, 20);
     }
 </style>
 <?php
 
-if(isset($_REQUEST["nome"]) && isset($_REQUEST["salario"]) && isset($_REQUEST["cargos"]) && isset($_REQUEST["opcoes"])){
+if (isset($_REQUEST["nome"]) && isset($_REQUEST["salario"]) && isset($_REQUEST["cargos"]) && isset($_REQUEST["opcoes"])) {
     $nome = $_REQUEST["nome"];
     $salario = $_REQUEST["salario"];
     $cargos = $_REQUEST["cargos"];
     $genero = $_REQUEST["opcoes"];
-    
-    if($salario > 5000){
-        $total = $salario * 110/100;
+
+    if ($salario > 5000) {
+        $total = $salario * 110 / 100;
         echo "<h1>A partir de alguns cálculos, $nome passará a receber R$$total, no cargo de $cargos. Aparentemente você também é do gênero '$genero'. Maneiro 👍</h1>";
-    }else{
-        $total = $salario * 120/100;
+    } else {
+        $total = $salario * 120 / 100;
         echo "<h1>A partir de alguns cálculos, $nome passará a receber R$$total, no cargo de $cargos. Aparentemente você também é do gênero '$genero'. Maneiro 👍 </h1>";
     }
-
-} 
+}
